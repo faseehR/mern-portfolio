eb2333435754605a5100db1a327698f19d6772e6
faseehR <faseeh.rahman321@gmail.com>
2026-07-27 22:28:25 -0700

    Backend Link Updated in Login/Signup

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -15,7 +15,7 @@ function Login() {
 
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/auth/login",
+        "https://task3backendportfolio.vercel.app/api/auth/login",
         {
           email,
           password,
